71a56fe51b79b1d4a6857acae17303787c52aeff
Tony <348281683@qq.com>
2017-09-08 21:06:31 +0800

    添加部分注释

--- Frontend/somecodes/notify.js
+++ Frontend/somecodes/notify.js
@@ -1,124 +1,160 @@
-  function notify(title, content) {  
-            
-          if(!title && !content){  
-              title = "桌面提醒";  
-              content = "您看到此条信息桌面提醒设置成功";  
-          }  
-          var iconUrl = "/images/send_ok.png";  
-            
-          if (window.webkitNotifications) {  
-              //chrome老版本  
-              if (window.webkitNotifications.checkPermission() == 0) {  
-                  var notif = window.webkitNotifications.createNotification(iconUrl, title, content);  
-                  notif.display = function() {}  
-                  notif.onerror = function() {}  
-                  notif.onclose = function() {}  
-                  notif.onclick = function() {this.cancel();}  
-                  notif.replaceId = 'Meteoric';  
-                  notif.show();  
-              } else {  
-                  window.webkitNotifications.requestPermission($jy.notify);  
+/**
+ *  桌面通知
+ *  title:标题
+ *  content:内容
+ */
+function notify(title, content) {     
+  if(!title && !content){  
+      title = "桌面提醒";  
+      content = "您看到此条信息桌面提醒设置成功";  
+  }  
+  var iconUrl = "/images/send_ok.png";  
+    
+  if (window.webkitNotifications) {  
+      //chrome老版本  
+      if (window.webkitNotifications.checkPermission() == 0) {  
+          var notif = window.webkitNotifications.createNotification(iconUrl, title, content);  
+          notif.display = function() {}  
+          notif.onerror = function() {}  
+          notif.onclose = function() {}  
+          notif.onclick = function() {this.cancel();}  
+          notif.replaceId = 'Meteoric';  
+          notif.show();  
+      } else {  
+          window.webkitNotifications.requestPermission($jy.notify);  
+      }  
+  }  
+  else if("Notification" in window){  
+      // 判断是否有权限  
+      if (Notification.permission === "granted") {  
+          var notification = new Notification(title, {  
+              "icon": iconUrl,  
+              "body": content,  
+          });  
+      }  
+      //如果没权限，则请求权限  
+      else if (Notification.permission !== 'denied') {  
+          Notification.requestPermission(function(permission) {  
+              // Whatever the user answers, we make sure we store the  
+              // information  
+              if (!('permission' in Notification)) {  
+                  Notification.permission = permission;  
               }  
-          }  
-          else if("Notification" in window){  
-              // 判断是否有权限  
-              if (Notification.permission === "granted") {  
+              //如果接受请求  
+              if (permission === "granted") {  
                   var notification = new Notification(title, {  
                       "icon": iconUrl,  
                       "body": content,  
                   });  
               }  
-              //如果没权限，则请求权限  
-              else if (Notification.permission !== 'denied') {  
-                  Notification.requestPermission(function(permission) {  
-                      // Whatever the user answers, we make sure we store the  
-                      // information  
-                      if (!('permission' in Notification)) {  
-                          Notification.permission = permission;  
-                      }  
-                      //如果接受请求  
-                      if (permission === "granted") {  
-                          var notification = new Notification(title, {  
-                              "icon": iconUrl,  
-                              "body": content,  
-                          });  
-                      }  
-                  });  
-              }  
-          }  
+          });  
       }  
+  }  
+}  
 
+/**
+ *  记录并通知符合购买条件的游戏
+ */
+function logAndNotify(gameName, playWay, key, value){
+  console.error('['+gameName+'] '+playWay+', '+key+'---'+value+', 符合购买条件！');
+  notify(gameName+'-'+playWay, key+'---'+value+', 符合购买条件！');
+}
 
-  function logAndNotify(gameName, playWay, key, value){
-    console.error('['+gameName+'] '+playWay+', '+key+'---'+value+', 符合购买条件！');
-    notify(gameName+'-'+playWay, key+'---'+value+', 符合购买条件！');
+/**
+ *  记录并通知开奖的游戏
+ */
+function logAndNotifyOpen(title, content){
+  console.error('['+title+']-'+content);
+  notify(title, content);
+}
+
+/**  =====================核心============== **/
+
+/**
+ *  解析11选5 游戏结果
+ *  分析 任一 间隔阈值
+ *    到达阈值时
+ *         发出购买提醒
+ *         监控开奖结果，为下次做准备
+ *  分析 定位 间隔阈值
+ *         发出购买提醒
+ *         监控开奖结果，为下次做准备
+ *   
+ *
+ */
+function parse11x5(data){
+  console.log('=====分析11选5任一=======');  
+  var all = data['all'];
+  for(var key in all){
+    var value = all[key]['m'];
+    console.log('分析:'+key+', 间隔值为：'+ value);
+    if(value > GLOBAL_CONFIG.THRESHOLD['11x5']['r1']){
+      logAndNotify(currentGame.name, '任一', key, value);
+
+      monitor11x5R1(currentGame.id, key, -100);
+    } 
   }
 
-  function logAndNotifyOpen(title, content){
-    console.error('['+title+']-'+content);
-    notify(title, content);
-  }
+  console.log('=====分析分析11选5定位=======');
 
-  function parse11x5(data){
-    console.log('=====分析任一=======');
-    // data = JSON.parse(data);
-    var all = data['all'];
-    for(var key in all){
-      var value = all[key]['m'];
-      console.log('分析:'+key+', 间隔值为：'+ value);
-      if(value > GLOBAL_CONFIG.THRESHOLD['11x5']['r1']){
-        logAndNotify(currentGame.name, '任一', key, value);
+  var oneData = data['one'];
+  for(var i = 0; i<oneData.length; i++){
+    var data = oneData[i];
+    console.log('分析第'+(i+1)+'位');
+    for(var key in data){
+      var value = data[key]['m'];
+      if(i < 3 && value > GLOBAL_CONFIG.THRESHOLD['11x5']['dw']){
+        logAndNotify(currentGame.name, '定位-'+(i+1), key, value);     
 
-        monitor11x5R1(currentGame.id, key, -100);
-      } 
-    }
+        monitor11x5Dw(currentGame.id, i+1, key, -100);
 
-    console.log('=====分析定位=======');
-
-    var oneData = data['one'];
-    for(var i = 0; i<oneData.length; i++){
-      var data = oneData[i];
-      console.log('分析第'+(i+1)+'位');
-      for(var key in data){
-        var value = data[key]['m'];
-        if(i < 3 && value > GLOBAL_CONFIG.THRESHOLD['11x5']['dw']){
-          logAndNotify(currentGame.name, '定位-'+(i+1), key, value);     
-
-          monitor11x5Dw(currentGame.id, i+1, key, -100);
-
-        }
       }
     }
   }
+}
 
-  function parsepk10(data){
-    console.log('=====分析北京赛车定位=======');
-    // data = JSON.parse(data);
-    var oneData = data['one'];
-    for(var i = 0; i<oneData.length; i++){
-      var data = oneData[i];
-      console.log('分析第'+(i+1)+'位');
-      for(var key in data){
-        var value = data[key]['m'];
-        if(value > GLOBAL_CONFIG.THRESHOLD['pk10']['dw']){
-          logAndNotify('北京赛车', '定位-'+(i+1), key, value);
-          monitorPk10OpenGame(key, value, -100);
-        }
+/**
+ *  解析11选5 游戏结果
+ *  分析 任一 间隔阈值
+ *    到达阈值时
+ *         发出购买提醒
+ *         监控开奖结果，为下次做准备
+ *  分析 定位 间隔阈值
+ *         发出购买提醒
+ *         监控开奖结果，为下次做准备
+ *   
+ *
+ */
+function parsepk10(data){
+  console.log('=====分析北京赛车定位=======');
+  var oneData = data['one'];
+  for(var i = 0; i<oneData.length; i++){
+    var data = oneData[i];
+    console.log('分析第'+(i+1)+'位');
+    for(var key in data){
+      var value = data[key]['m'];
+      if(value > GLOBAL_CONFIG.THRESHOLD['pk10']['dw']){
+        logAndNotify('北京赛车', '定位-'+(i+1), key, value);
+        monitorPk10OpenGame(key, value, -100);
       }
     }
   }
+}
 
+  /** 11选5 当前游戏 **/
   var currentGame = null;
+  /** 11选5 当前游戏编号 **/
   var currentIndex = 7;
 
-  var GLOBAL_CONFIG = {
+  /** 游戏全局配置 **/
+  var GLOBAL_CONFIG = {    
     GAMES : {
       '6' :{
-        id :6,
+        id :6,                  //游戏编号
         name: '北京赛车',
-        url: 'http://cbou1.diusx.com:880/z/gCAR-analyzeAjax',
-        data: {gi : 6},
-        latestResponse : ''
+        url: 'http://cbou1.diusx.com:880/z/gCAR-analyzeAjax',   //游戏分析数据获取接口
+        data: {gi : 6},                                         //请求参数
+        latestResponse : ''                                     //最后一次请求，用于过滤重复请求解析
       },
       '7' :{
         id :7,
@@ -151,15 +187,22 @@
     },
     THRESHOLD : {
       '11x5' : {
-        'r1' : 12,
-        'dw' : 75
+        'r1' : 12,        //11选5 任一 间隔阈值
+        'dw' : 75         //11选5 定位 间隔阈值
       },
-      'pk10' : {
-        'dw' : 80
+      'pk10' : {         
+        'dw' : 80         //北京赛车 定位 间隔阈值
       }
     }
   }
 
+  /**
+   *  请求数据解析总入口
+   *  检测 预测情况
+   *  检测 开奖情况
+   *  检测 自定义设置情况
+   *
+   */
   function runResponse(game_id, responseData, callback){
     if(GLOBAL_CONFIG.GAMES[game_id]['latestResponse'] == responseData){
       return;
